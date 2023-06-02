9139a9a26cd1a6bf4ced3a918868e87224002b46
shuita03 <599867514@qq.com>
2023-06-02 12:27:34 +0800

    delete api

--- 05-parctice/common/assert_util.py
+++
@@ -1,6 +0,0 @@
-
-class AssretUtil(object):
-
-    @classmethod
-    def assert_login(cls,msg,response):
-        assert msg == response.json().get('msg')
--- 05-parctice/config.py
+++
@@ -1,5 +0,0 @@
-# 导包
-import os
-
-# 获取上一级路径
-BASE_DIR = os.path.dirname(__file__)
